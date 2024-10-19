91595499aab58ed97ff6fef94cc8192281a21dc6
Mattster <mats-hjalmar.murman@alster.se>
2024-10-19 11:47:32 +0200

    Update Docs

--- ui-state/src/commonMain/kotlin/se/alster/ui_state/UiState.kt
+++ ui-state/src/commonMain/kotlin/se/alster/ui_state/UiState.kt
@@ -1,5 +1,18 @@
 package se.alster.ui_state
 
+/**
+ * Represents the state of a UI component. It can be in one of the following states:
+ * - [Idle] - The initial state of the UI component.
+ * - [Loading] - The UI component is loading data.
+ * - [Data] - The UI component has received data.
+ * - [Error] - An error occurred while loading data.
+ * @param T The type of the data.
+ * @see Result
+ * @see toUiState
+ * @see toUiStateData
+ * @see mapData
+ * @see dataOrNull
+ */
 sealed interface UiState<out T> {
     data object Idle : UiState<Nothing>
     data object Loading : UiState<Nothing>
@@ -7,6 +20,9 @@ sealed interface UiState<out T> {
     data class Error(val value: Throwable) : UiState<Nothing>
 }
 
+/**
+ * Converts a [Result] to a [UiState].
+ */
 fun <T> Result<T>.toUiState(): UiState<T> {
     onSuccess {
         return UiState.Data(it)
@@ -17,10 +33,16 @@ fun <T> Result<T>.toUiState(): UiState<T> {
     return UiState.Loading
 }
 
+/**
+ * Converts a value to a [UiState.Data].
+ */
 fun <T : Any> T.toUiStateData(): UiState.Data<T> {
     return UiState.Data(this)
 }
 
+/**
+ * Maps the data of a [UiState] to another type.
+ */
 fun <T, R> UiState<T>.mapData(data: (T) -> R): UiState<R> {
     return when (this) {
         is UiState.Data -> UiState.Data(data(value))
@@ -30,6 +52,9 @@ fun <T, R> UiState<T>.mapData(data: (T) -> R): UiState<R> {
     }
 }
 
+/**
+ * Returns the data of a [UiState] or `null` if the state is not [UiState.Data].
+ */
 fun <T> UiState<T>.dataOrNull(): T? {
     return when (this) {
         is UiState.Data -> value
